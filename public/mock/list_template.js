1ae3edd56d542c8f2374dba28cfd9b3bf89dcfbd
primayudantra <prima.yudantra@gmail.com>
2015-08-13 12:08:34 +0700

    Almost Clear

---
+++ public/mock/list_template.js
@@ -0,0 +1,53 @@
+exports.template_list = [
+		
+		{
+			"id" : "Template 1",
+			name: "Template 1"
+		},
+		{
+			"id" : "Template 2",
+			name: "Template 2"
+		},
+		{
+			"id" : "Template 3",
+			name: "Template 3"
+		},
+		{
+			"id" : "Template 4",
+			name: "Template 4"
+		}
+]
+
+exports.tipe_pengiriman = [
+	{
+		"id" : "Sehari 1x",
+		name: "Sehari 1x"
+	},
+	{
+		"id" : "Seminggu 1x",
+		name: "Seminggu 1x"
+	},
+	{
+		"id" : "Sebulan 1x",
+		name: "Sebulan 1x"
+	},
+	{
+		"id" : "Custom",
+		name: "Custom"
+	}
+]
+
+exports.tipe_engine = [
+	{
+		"id" : "Mail Engine",
+		name: "Mail Engine"
+	},
+	{
+		"id" : "Haraka",
+		name: "Haraka"
+	},
+	{
+		"id" : "Mandrill",
+		name: "Mandrill"
+	}
+]
